bf409f2f85fde2665928a4b3925d02c1ff8023f1
Arran Schlosberg <me@arranschlosberg.com>
2024-09-12 20:53:39 +0100

    fix: check out branch before performing diff

--- .github/workflows/diff_from_libevm-base.sh
+++ .github/workflows/diff_from_libevm-base.sh
@@ -2,6 +2,7 @@
 
 set -u;
 
+git checkout "${1}";
 git diff --diff-filter=a --word-diff --unified=0 libevm-base.."${1}" \
     ':(exclude).golangci.yml' \
-    ':(exclude).github/**'
+    ':(exclude).github/**';
