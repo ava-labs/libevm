8ae52e4b9628828a35be5915671f1c700562a311
Arran Schlosberg <me@arranschlosberg.com>
2024-09-12 20:58:18 +0100

    fix: add `--` suffix to `git checkout`

--- .github/workflows/diff_from_libevm-base.sh
+++ .github/workflows/diff_from_libevm-base.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -ux;
+set -eux;
 
-git checkout "${1}";
+git checkout "${1}" --;
 git diff --diff-filter=a --word-diff --unified=0 libevm-base.."${1}" \
     ':(exclude).golangci.yml' \
     ':(exclude).github/**';
