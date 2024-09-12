d47a4f8e3eb0dccc24af56cd2592d0cbfb9f31d7
Arran Schlosberg <me@arranschlosberg.com>
2024-09-12 14:37:36 +0100

    chore: this is trying my patience

--- params/example.libevm_test.go
+++ params/example.libevm_test.go
@@ -106,7 +106,7 @@ func (r RulesExtra) PrecompileOverride(addr common.Address) (_ libevm.Precompile
 // to state allows it to be configured on-chain however this is an optional
 // implementation detail.
 func (r RulesExtra) CanCreateContract(*libevm.AddressContext, libevm.StateReader) error {
-	if time.Unix(int64(r.timestamp), 0).UTC().Day() != int(time.Tuesday) { // #nosec G115
+	if time.Unix(int64(r.timestamp), 0).UTC().Day() != int(time.Tuesday) { //nolint:gosec // triggering G115 overflow
 		return errors.New("uh oh")
 	}
 	return nil
