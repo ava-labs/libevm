110b6696e0e1248e98980d64e0574a6bf1789cab
Arran Schlosberg <me@arranschlosberg.com>
2024-09-12 21:11:05 +0100

    refactor: move all actions inside workflow

--- .github/workflows/diff_from_libevm-base.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux;
-
-git checkout "${1}" --;
-git diff --diff-filter=a --word-diff --unified=0 libevm-base.."${1}" \
-    ':(exclude).golangci.yml' \
-    ':(exclude).github/**';
