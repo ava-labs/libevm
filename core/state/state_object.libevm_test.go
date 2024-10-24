6abb19e3089b427a5628f53c9cf7d8789413b793
Arran Schlosberg <me@arranschlosberg.com>
2024-10-24 17:28:11 +1100

    chore: sshhhh linter, sshhhh

--- core/state/state_object.libevm_test.go
+++ core/state/state_object.libevm_test.go
@@ -1,11 +1,28 @@
+// Copyright 2024 the libevm authors.
+//
+// The libevm additions to go-ethereum are free software: you can redistribute
+// them and/or modify them under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// The libevm additions are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+// General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see
+// <http://www.gnu.org/licenses/>.
+
 package state
 
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/core/types"
-	"github.com/stretchr/testify/require"
 )
 
 func TestStateObjectEmpty(t *testing.T) {
