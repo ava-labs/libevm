f8237711250069ddc1b281faf0e84ebd9d850d2b
Arran Schlosberg <me@arranschlosberg.com>
2024-11-18 16:16:31 +0000

    refactor: remove user-supplied gas limit for `env.Create()`

--- core/vm/environment.libevm.go
+++ core/vm/environment.libevm.go
@@ -99,10 +99,6 @@ func (e *environment) BlockHeader() (types.Header, error) {
 	return *hdr, nil
 }
 
-func (e *environment) Call(addr common.Address, input []byte, gas uint64, value *uint256.Int, opts ...CallOption) ([]byte, error) {
-	return e.callContract(Call, addr, input, gas, value, opts...)
-}
-
 func (e *environment) beforeNewCallFrame(typ CallType, gas uint64, value *uint256.Int, opts ...CallOption) (ContractRef, error) {
 	var caller ContractRef = e.self
 	if options.As[callConfig](opts...).unsafeCallerAddressProxying {
@@ -127,6 +123,14 @@ func (e *environment) beforeNewCallFrame(typ CallType, gas uint64, value *uint25
 	return caller, nil
 }
 
+func (e *environment) afterNewCallFrame(returnGas uint64) error {
+	return e.refundGas(returnGas)
+}
+
+func (e *environment) Call(addr common.Address, input []byte, gas uint64, value *uint256.Int, opts ...CallOption) ([]byte, error) {
+	return e.callContract(Call, addr, input, gas, value, opts...)
+}
+
 func (e *environment) callContract(typ CallType, addr common.Address, input []byte, gas uint64, value *uint256.Int, opts ...CallOption) ([]byte, error) {
 	caller, err := e.beforeNewCallFrame(typ, gas, value, opts...)
 	if err != nil {
@@ -136,7 +140,7 @@ func (e *environment) callContract(typ CallType, addr common.Address, input []by
 	switch typ {
 	case Call:
 		ret, returnGas, callErr := e.evm.Call(caller, addr, input, gas, value)
-		if err := e.refundGas(returnGas); err != nil {
+		if err := e.afterNewCallFrame(returnGas); err != nil {
 			return nil, err
 		}
 		return ret, callErr
@@ -153,28 +157,33 @@ func (e *environment) callContract(typ CallType, addr common.Address, input []by
 	}
 }
 
-func (e *environment) Create(code []byte, gas uint64, value *uint256.Int) ([]byte, common.Address, error) {
-	return e.create(create, gas, value, func(caller ContractRef) ([]byte, common.Address, uint64, error) {
+func (e *environment) Create(code []byte, value *uint256.Int) ([]byte, common.Address, error) {
+	return e.create(create, value, func(caller ContractRef, gas uint64) ([]byte, common.Address, uint64, error) {
 		return e.evm.Create(caller, code, gas, value)
 	})
 }
 
-func (e *environment) Create2(code []byte, gas uint64, value, salt *uint256.Int) ([]byte, common.Address, error) {
-	return e.create(create2, gas, value, func(caller ContractRef) ([]byte, common.Address, uint64, error) {
+func (e *environment) Create2(code []byte, value, salt *uint256.Int) ([]byte, common.Address, error) {
+	return e.create(create2, value, func(caller ContractRef, gas uint64) ([]byte, common.Address, uint64, error) {
 		return e.evm.Create2(caller, code, gas, value, salt)
 	})
 }
 
-type creator func(ContractRef) ([]byte, common.Address, uint64, error)
+type creator func(ContractRef, uint64) ([]byte, common.Address, uint64, error)
 
-func (e *environment) create(typ CallType, gas uint64, value *uint256.Int, do creator) ([]byte, common.Address, error) {
+func (e *environment) create(typ CallType, value *uint256.Int, do creator) ([]byte, common.Address, error) {
+	gas := e.Gas()
+	if e.Rules().IsEIP150 {
+		gas -= gas / 64
+	}
+
 	caller, err := e.beforeNewCallFrame(typ, gas, value)
 	if err != nil {
 		return nil, common.Address{}, err
 	}
 
-	ret, contract, returnGas, err := do(caller)
-	if err := e.refundGas(returnGas); err != nil {
+	ret, contract, returnGas, err := do(caller, gas)
+	if err := e.afterNewCallFrame(returnGas); err != nil {
 		return nil, common.Address{}, err
 	}
 	return ret, contract, err
