a0fe5a6c42344685537498610586e6ad05508b9c
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-04-10 21:49:34 +0300

    chore: release v1.13.14-0.2.0.release

--- params/version.libevm.go
+++ params/version.libevm.go
@@ -21,8 +21,8 @@ const (
 	LibEVMVersionMinor = 2
 	LibEVMVersionPatch = 0
 
-	LibEVMReleaseType      ReleaseType = BetaRelease
-	libEVMReleaseCandidate uint        = 3 // ignored unless [LibEVMReleaseType] == [ReleaseCandidate]
+	LibEVMReleaseType      ReleaseType = ProductionRelease
+	libEVMReleaseCandidate uint        = 0 // ignored unless [LibEVMReleaseType] == [ReleaseCandidate]
 )
 
 // LibEVMVersion holds the textual version string of `libevm` modifications.
@@ -48,7 +48,7 @@ const (
 // triplet.
 //
 // [semver v2]: https://semver.org/
-const LibEVMVersion = "1.13.14-0.2.0.beta"
+const LibEVMVersion = "1.13.14-0.2.0.release"
 
 // A ReleaseType is a suffix for [LibEVMVersion].
 type ReleaseType string
