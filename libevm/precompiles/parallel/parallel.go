3669967032de1e647ac45d7a9388f15d6e568450
Arran Schlosberg <me@arranschlosberg.com>
2025-09-15 13:16:58 +0100

    chore: copyright headers + package comment

--- libevm/precompiles/parallel/parallel.go
+++ libevm/precompiles/parallel/parallel.go
@@ -1,3 +1,21 @@
+// Copyright 2025 the libevm authors.
+//
+// The libevm additions to go-ethereum are free software: you can redistribute
+// them and/or modify them under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// The libevm additions are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+// General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see
+// <http://www.gnu.org/licenses/>.
+
+// Package parallel provides functionality for precompiled contracts that can
+// pre-process their results in an embarrassingly parallel fashion.
 package parallel
 
 import (
