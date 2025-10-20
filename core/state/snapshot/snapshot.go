2f703980e838e72e0a7c347d85d49b0c7d2b22b8
Arran Schlosberg <me@arranschlosberg.com>
2025-10-20 12:22:49 +0100

    Revert "core/state/snapshot: add a missing lock (#30001)"
    This reverts commit 7688f988c243d9fa588d12a1a1bf0e7017c2a387.

--- core/state/snapshot/snapshot.go
+++ core/state/snapshot/snapshot.go
@@ -832,8 +832,6 @@ func (t *Tree) disklayer() *diskLayer {
 	case *diskLayer:
 		return layer
 	case *diffLayer:
-		layer.lock.RLock()
-		defer layer.lock.RUnlock()
 		return layer.origin
 	default:
 		panic(fmt.Sprintf("%T: undefined layer", snap))
@@ -865,7 +863,7 @@ func (t *Tree) generating() (bool, error) {
 	return layer.genMarker != nil, nil
 }
 
-// DiskRoot is an external helper function to return the disk layer root.
+// DiskRoot is a external helper function to return the disk layer root.
 func (t *Tree) DiskRoot() common.Hash {
 	t.lock.Lock()
 	defer t.lock.Unlock()
