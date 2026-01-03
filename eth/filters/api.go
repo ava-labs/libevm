fe93e1e800b04efd29ffa436044bbdcd36158497
Stephen Buttolph <stephen@avalabs.org>
2026-01-03 15:18:00 -0500

    Reduce diff

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -25,7 +25,7 @@ import (
 	"sync"
 	"time"
 
-	ethereum "github.com/ava-labs/libevm"
+	"github.com/ava-labs/libevm"
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/common/hexutil"
 	"github.com/ava-labs/libevm/core/types"
