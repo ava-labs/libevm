c3d9f415dcc7c4e92ea5f5d01a2664fcceb0f6fb
Austin Larson <austin.larson@avalabs.org>
2026-01-30 12:53:40 -0500

    feat: export some constants

--- eth/bloombits.libevm.go
+++ eth/bloombits.libevm.go
@@ -21,6 +21,20 @@ import (
 	"github.com/ava-labs/libevm/ethdb"
 )
 
+const (
+	// BloomFilterThreads is the number of goroutines used locally per filter to
+	// multiplex requests onto the global servicing goroutines.
+	BloomFilterThreads = bloomFilterThreads
+
+	// BloomRetrievalBatch is the maximum number of bloom bit retrievals to service
+	// in a single batch.
+	BloomRetrievalBatch = bloomRetrievalBatch
+
+	// BloomRetrievalWait is the maximum time to wait for enough bloom bit requests
+	// to accumulate request an entire batch (avoiding hysteresis).
+	BloomRetrievalWait = bloomRetrievalWait
+)
+
 // StartBloomHandlers starts a batch of goroutines to accept bloom bit database
 // retrievals from possibly a range of filters and serving the data to satisfy.
 // This is identical to [Ethereum.startBloomHandlers], but exposed for use separately.
