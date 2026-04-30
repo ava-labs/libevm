c891ff86e981d893ddc3bf9dfe07e0f0b05037c4
Austin Larson <78000745+alarso16@users.noreply.github.com>
2026-04-30 17:04:57 -0400

    feat(state): Intercept `Database` creation (#283)
    ## Why this should be merged
    
    For Firewood, there's many places in libevm where it uses these methods
    to construct a `state.Database`, but we need a different implementation.
    Since this is separate from the `StateDB` interfaces (I don't want to
    have to register for each test, but rather in a single `init` function),
    I made it a separate registration for simplicity.
    
    The ultimate goal is for ava-labs/avalancheg#5314, so if this approach
    is not correct to enable that, this PR should not be merged.
    
    ## How this works
    
    Intercept each creation with the possibility of wrapping the state cache
    if it is registered.
    
    ## How this was tested
    
    Added some no-op unit tests.
    
    ---------
    
    Co-authored-by: Arran Schlosberg <519948+ARR4N@users.noreply.github.com>

--- core/types/rlp_payload.libevm.go
+++ core/types/rlp_payload.libevm.go
@@ -109,8 +109,9 @@ func payloadsAndConstructors[
 //
 // This MUST NOT be used on a live chain. It is solely intended for off-chain
 // consumers that require access to extras. Said consumers SHOULD NOT, however
-// call this function directly. Use the libevm/temporary.WithRegisteredExtras()
-// function instead as it atomically overrides all possible packages.
+// call this function directly. Use the [libevm.WithTemporaryExtrasLock]
+// function instead in combination with all other registrations to ensure
+// that temporary registrations are atomically applied.
 func WithTempRegisteredExtras[
 	H, B, SA any,
 	HPtr HeaderHooksPointer[H],
