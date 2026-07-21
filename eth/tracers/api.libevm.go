7fe6eb0cb2cab723d79d756fa6c6ab61a4d586c3
Austin Larson <78000745+alarso16@users.noreply.github.com>
2026-07-21 11:00:43 -0400

    feat(eth/tracers): Allow header overwrites (#295)
    ## Why this should be merged
    
    The `types.Header` must be altered in SAE to return the appropriate
    state root and base fee, so that the API can accurately find the these
    values for execution. However, this will modify the block hash (since
    this is found runtime in the header), so this provides the option to
    overwrite the block hash via an optional interface. Instead of getting
    the block hash from the header, an implementer can retrieve the block
    again, and use the original hash instead.
    
    ## How this works
    
    Use optional interface for every call of `block.Hash()`
    
    ## How this was tested
    
    New UT
    
    ---------
    
    Signed-off-by: Tsvetan Dimitrov (tsvetan.dimitrov@avalabs.org)
    Co-authored-by: Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>

---
+++ eth/tracers/api.libevm.go
@@ -0,0 +1,42 @@
+// Copyright 2026 the libevm authors.
+//
+// The libevm additions to go-ethereum are free software: you can redistribute
+// them and/or modify them under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// The libevm additions are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+// General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see
+// <http://www.gnu.org/licenses/>.
+
+package tracers
+
+import (
+	"github.com/ava-labs/libevm/common"
+	"github.com/ava-labs/libevm/core/types"
+)
+
+// BlockHashOverrider is an optional extension to [Backend], allowing an
+// arbitrary block hash to be returned for a block. If not implemented,
+// [types.Block.Hash] is used instead.
+//
+// This would be used in the case that the [types.Header] is manipulated in the
+// [Backend] for tracer-specific behavior. However, all hashes directly
+// returned by the [Backend] are expected to be correct.
+type BlockHashOverrider interface {
+	// BlockHash returns the hash of the provided block.
+	BlockHash(*types.Block) common.Hash
+}
+
+func (a *API) blockHash(block *types.Block) common.Hash {
+	overrider, ok := a.backend.(BlockHashOverrider)
+	if !ok {
+		return block.Hash()
+	}
+	return overrider.BlockHash(block)
+}
